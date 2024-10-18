72ab2bd6f05e8e9eafd46e3a6bc40feb4c3320fc
kanesoban <kanesoban@gmail.com>
2024-10-18 08:01:24 +0200

    Created baseline training script and config

--- src/disaster_tweets/scripts/train.py
+++ src/disaster_tweets/scripts/train.py
@@ -1,150 +1,89 @@
+"""Defines entrypoint for training."""
 import argparse
+import csv
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
-import torch
 import yaml
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, Dataset
-from transformers import AdamW, BertForSequenceClassification, BertTokenizer
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, f1_score, precision_score, recall_score
 
-
-class TextClassificationDataset(Dataset):
-    def __init__(self, text, target, tokenizer, max_length):
-        self.text = text
-        self.target = target
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-
-    def __len__(self):
-        return len(self.text)
-
-    def __getitem__(self, idx):
-        text_encoded = self.tokenizer.encode_plus(
-            self.text[idx], max_length=self.max_length, padding="max_length", truncation=True
-        )
-        return {
-            "input_ids": torch.tensor(text_encoded["input_ids"], dtype=torch.long),
-            "attention_mask": torch.tensor(text_encoded["attention_mask"], dtype=torch.long),
-            "labels": torch.tensor(self.target[idx], dtype=torch.long),
-        }
-
-
-class TextClassifier(pl.LightningModule):
-    def __init__(self, config):
-        super().__init__()
-        self.model = BertForSequenceClassification.from_pretrained(config["bert_model"])
-        self.tokenizer = BertTokenizer.from_pretrained(config["bert_model"])
-        self.loss = torch.nn.CrossEntropyLoss()
-        self.config = config
-
-    def forward(self, input_ids, attention_mask):
-        output = self.model(input_ids=input_ids, attention_mask=attention_mask)
-        return output.logits
-
-    def training_step(self, batch, batch_idx):
-        logits = self(batch["input_ids"], batch["attention_mask"])
-        loss = self.loss(logits, batch["labels"])
-        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        logits = self(batch["input_ids"], batch["attention_mask"])
-        loss = self.loss(logits, batch["labels"])
-        self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
-
-    def configure_optimizers(self):
-        return AdamW(self.parameters(), lr=self.config["learning_rate"])
-
-
-class ClassifierDataModule(pl.LightningDataModule):
-    def __init__(self, config):
-        super().__init__()
-        self.config = config
-
-    def setup(self, stage=None):
-        # Load data
-        train = pd.read_csv(self.config["data_path"] + "/train.csv")
-        test = pd.read_csv(self.config["data_path"] + "/test.csv")
-
-        # Split train data into train and validation sets
-        train, val = train_test_split(train, test_size=self.config["validation_split"])
-
-        # Define tokenizer
-        self.tokenizer = BertTokenizer.from_pretrained(self.config["bert_model"])
-
-        # Create datasets
-        self.train_dataset = TextClassificationDataset(
-            train["text"], train["target"], self.tokenizer, self.config["max_seq_length"]
-        )
-
-        self.validation_dataset = TextClassificationDataset(
-            val["text"], val["target"], self.tokenizer, self.config["max_seq_length"]
-        )
-        self.test_dataset = TextClassificationDataset(
-            test["text"], test["target"], self.tokenizer, self.config["max_seq_length"]
-        )
-
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.config["batch_size"],
-            shuffle=True,
-            num_workers=self.config["num_workers"],
-        )
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.validation_dataset,
-            batch_size=self.config["batch_size"],
-            shuffle=False,
-            num_workers=self.config["num_workers"],
-        )
-
-    def test_dataloader(self):
-        return DataLoader(
-            self.test_dataset,
-            batch_size=self.config["batch_size"],
-            shuffle=False,
-            num_workers=self.config["num_workers"],
-        )
+from disaster_tweets.data import ClassifierDataModule
+from disaster_tweets.model import TextClassifier
+from disaster_tweets.training import EarlyStoppingWithMinEpochs
 
 
 def main(config):
-    model = TextClassifier(config)
+    """Run main function."""
     data_module = ClassifierDataModule(config)
+    train_df = data_module.train_df
+    class_counts = train_df["target"].value_counts()
+    total_samples = len(train_df)
+    class_weights = {cls: total_samples / count for cls, count in class_counts.items()}
+    classes = list(class_weights.keys())
+    model = TextClassifier(config, class_weights=[class_weights[cls] for cls in classes])
 
-    trainer = pl.Trainer(gpus=config["gpus"], max_epochs=config["num_epochs"], default_root_dir=config["output_dir"])
+    early_stopping_with_min_epochs = EarlyStoppingWithMinEpochs(
+        min_epochs=config["min_epochs"], patience=config["patience"], monitor="val_loss", mode="min", verbose=True
+    )
+
+    trainer = pl.Trainer(
+        accelerator="gpu",
+        devices=1,
+        max_epochs=config["max_epochs"],
+        default_root_dir=config["output_dir"],
+        callbacks=[early_stopping_with_min_epochs],
+        logger=True,
+    )
     trainer.fit(model, data_module)
 
     # Predicting on test and train datasets
-    test_predictions = trainer.predict(model, datamodule=data_module.test_dataloader())
-    train_predictions = trainer.predict(model, datamodule=data_module.train_dataloader())
+    val_predictions = trainer.predict(model, dataloaders=data_module.val_dataloader())
+
+    # Calculate metrics
+    pred = np.concatenate([batch.argmax(axis=-1) for batch in val_predictions])
+    true_labels = data_module.validation_dataset.target
+    accuracy = (pred == true_labels).mean()
+    precision = precision_score(true_labels, pred)
+    recall = recall_score(true_labels, pred)
+    f1 = f1_score(true_labels, pred)
+
+    metrics = {
+        "accuracy": float(accuracy),
+        "precision": float(precision),
+        "recall": float(recall),
+        "f1_score": float(f1),
+    }
+
+    # Save metrics to a file
+    with open(config["output_dir"] + "/metrics.yaml", "w") as f:
+        yaml.dump(metrics, f)
 
     # Confusion matrix for test dataset
-    cm_test = confusion_matrix(data_module.test_dataset.target, np.argmax(test_predictions, axis=-1))
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm_test, display_labels=["NEG", "POS"])
+    cm_val = confusion_matrix(true_labels, pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm_val, display_labels=["Not disaster", "Disaster"])
     disp.plot()
 
     plt.title("Test Confusion Matrix")
-    plt.savefig(config["output_dir"] + "/test_cm.png")
+    plt.savefig(config["output_dir"] + "/val_cm.png")
 
     # Plot train and validation losses
     plt.figure()
-    plt.plot(trainer.logged_metrics["train_loss"], label="Train Loss")
-    plt.plot(trainer.logged_metrics["val_loss"], label="Validation Loss")
+    losses = np.array(model.train_losses) / model.train_losses[0]
+    plt.plot(losses, label="Train Loss")
+    losses = np.array(model.val_losses) / model.val_losses[0]
+    plt.plot(losses, label="Validation Loss")
     plt.title("Train vs Validation Loss")
     plt.legend()
     plt.savefig(config["output_dir"] + "/train_val_loss.png")
 
-    # Saving misclassified samples for train dataset
-    misclassified = np.argmax(train_predictions, axis=-1) != data_module.train_dataset.target
-    misclassified_df = data_module.train_dataset.iloc[misclassified]
-    misclassified_df.to_csv(config["output_dir"] + "/misclassified.csv", index=False)
+    # Saving misclassified samples for val dataset
+    train = pd.read_csv(config["data_path"] + "/train.csv")
+    misclassified = np.argmax(pred, axis=-1) != true_labels
+    misclassified_idx = np.array(data_module.validation_dataset.df_index)[misclassified]
+    misclassified_df = train.loc[misclassified_idx]
+    misclassified_df.to_csv(config["output_dir"] + "/misclassified.csv", sep=",", index=False, quoting=csv.QUOTE_ALL)
 
 
 if __name__ == "__main__":
